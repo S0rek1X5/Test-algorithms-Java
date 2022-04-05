691cb9236ecf5964671d88235cd9ae7a9c891ed5
Daniel <danielbebon@gmail.com>
2022-04-05 18:10:51 -0500

    Ordenando carpetas

--- Basic_Math/Prueba.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Prueba{
-    private Scanner sc;
-    public static void main(String[]args){
-        int sexo=15;
-        System.out.println("El profesor no sabe una shit " + sexo);
-        
-    }
-}
--- Basic_Math/README.md
+++
@@ -1,2 +0,0 @@
-# Test-algorithms-Java
- Algoritmos de prueba desarrollados en lenguaje java
