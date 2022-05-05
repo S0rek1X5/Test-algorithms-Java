5a4aa7d5989555d47c3b30f537cd44e04e0b8097
Daniel <danielbebon@gmail.com>
2022-05-05 11:34:15 -0500

    Actualizacion
    +Do while

--- Num_Entre/src/Main.java
+++ Num_Entre/src/Main.java
@@ -1,13 +1,16 @@
+import java.io.IOException;
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String... arg) throws IOException, InterruptedException {
         int n1, n2;
         Scanner sc=new Scanner(System.in);
-        System.out.print("Ingrese el primer numero: ");n1=sc.nextInt();
-        System.out.print("Ingrese el Segundo numero: ");n2=sc.nextInt();
-
-
-        Num_Entre Main=new Num_Entre();
-        Main.procesar(n1, n2);sc.close();
+        do{
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            System.out.print("Ingrese el primer numero: ");n1=sc.nextInt();
+            System.out.print("Ingrese el Segundo numero: ");n2=sc.nextInt();
+            
+            Num_Entre Main=new Num_Entre();
+            Main.procesar(n1, n2);sc.close();
+        }while((opc=='s') || (opc=='S'));sc.close();
     }
 }
